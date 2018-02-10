2110a27c3bf4a326d956fe50a7219098c16515e6
Chai Phonbopit <phonbopit@gmail.com>
2018-02-10 11:29:13 +0700

    Initial Commit

---
+++ AhoyMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AhoyMac
+//
+//  Created by Chai Phonbopit on 2/10/18.
+//  Copyright © 2018 Chai Phonbopit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AhoyMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AhoyMac
+//
+//  Created by Chai Phonbopit on 2/10/18.
+//  Copyright © 2018 Chai Phonbopit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
